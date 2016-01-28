a6be2f2479d3ad6aebda4f3d79ae51488ece9ef1
SebNickel <sebastianchristophnickel@gmail.com>
2016-01-28 21:27:31 +0100

    Minor style change

--- new_cards.py
+++ new_cards.py
@@ -1,7 +1,5 @@
 from new_card import new_card
 
 def new_cards(conn, cursor, two_way_card):
-    while True:
+    while two_way_card != None:
         two_way_card = new_card(conn, cursor, two_way_card)
-        if two_way_card == None:
-            break
