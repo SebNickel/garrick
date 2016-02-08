0510f3f9c012e88f5e240d555ada510b38554e7a
SebNickel <sebastianchristophnickel@gmail.com>
2016-02-08 11:03:06 +0100

    Fixed default_colors

--- default_colors.py
+++ default_colors.py
@@ -10,19 +10,19 @@ default_colors = ColoredOutput(
 )
 
 def default_print_info(string):
-    colored_output.print_info(string)
+    default_colors.print_info(string)
 
 def default_print_error(string):
-    colored_output.print_error(string)
+    default_colors.print_error(string)
 
 def default_print_instruction(string):
-    colored_output.print_instruction(string)
+    default_colors.print_instruction(string)
 
 def default_print_side(string):
-    colored_output.print_side(string)
+    default_colors.print_side(string)
 
 def default_colored_prompt(prompt):
-    return colored_output.colored_prompt(prompt)
+    return default_colors.colored_prompt(prompt)
 
 def default_colored_getpass(prompt):
-    colored_output.colored_getpass(prompt)
+    default_colors.colored_getpass(prompt)
